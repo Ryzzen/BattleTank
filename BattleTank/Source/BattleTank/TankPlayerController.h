9603b39aa3e03df992cc4045dc4cafec8d48085b
Ryzzen <a.mayaud@hotmail.fr>
2021-10-24 19:02:46 +0200

    Revert "Revert "[159] [ADD} (Crosshair) Bound crosshair color to a function""
    This reverts commit 5670a67453818afd2740e45c2508c2dbd8f3724c.

--- BattleTank/Source/BattleTank/TankPlayerController.h
+++ BattleTank/Source/BattleTank/TankPlayerController.h
@@ -28,9 +28,12 @@ public:
 	UPROPERTY(EditDefaultsOnly)
 	float ShootingRange = 100000;
 
+protected:
+	UFUNCTION(BlueprintCallable, Category = Setup)
+	ATank* GetControlledTank() const;
+
 private:
 	void AimTowardsCrosshair();
-	ATank* GetControlledTank() const;
 	bool GetSightRayHitLocation(FVector& HitLocation) const;
 	bool GetAimDirection(FVector& AimDirection) const;
 	bool GetAimVectorHitLocation(FVector& HitLocation, FVector AimDirection) const;
